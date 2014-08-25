3d06252686bf2827992ff3eea03d25dca39fe89e
lok117 <lok117@LOK117-01>
2014-08-25 09:17:45 +0200

    overview lijst

---
+++ overzicht.php
@@ -0,0 +1,29 @@
+<?php
+    require_once("module.php");
+    require_once("moduleLijst.php");
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset=utf-8>
+        <title>Modules</title>
+    </head>
+    <body>
+        <h1>Modules</h1>
+        <?php
+            $modLijst = new moduleLijst();
+            $tab = $modLijst->getLijst();
+        ?>
+        <ul>
+            <?php
+                foreach ($tab as $module) {
+                    $moduleNaam = $module->getNaam();
+                    $moduleId = $module->getId();
+                    print("<li>" . $moduleNaam . "(<a href=\"modulebewerken.php?id=". $moduleId . "\">Bewerken</a>) </li>");
+                }
+            ?>
+        </ul>
+    </body>
+</html>
+
+
